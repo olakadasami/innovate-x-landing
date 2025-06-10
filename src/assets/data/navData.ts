426a5cd137843e427d9e1e5d5a94c2e8d2fe5f24
Samuel Olakada <olakadasamuel@gmail.com>
2025-06-10 12:56:27 +0100

    chore: add navbar

---
+++ src/assets/data/navData.ts
@@ -0,0 +1,9 @@
+export const navData = [
+  { href: "/", title: "Logo" },
+  { href: "/schedule", title: "Schedule" },
+  { href: "/speakers", title: "Speakers" },
+  { href: "/sponsors", title: "Sponsors" },
+  { href: "/faq", title: "FAQ" },
+  { href: "/venue", title: "Venue" },
+  { href: "/register", title: "Register" },
+];
