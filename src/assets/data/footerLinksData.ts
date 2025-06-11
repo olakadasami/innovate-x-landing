a18ffade2ceac1fecc8e291a0e5cfbaadebf379b
Samuel Olakada <olakadasamuel@gmail.com>
2025-06-11 15:23:15 +0100

    feat: add footer

---
+++ src/assets/data/footerLinksData.ts
@@ -0,0 +1,36 @@
+import instagram from "../images/svgs/instagram.svg";
+import facebook from "../images/svgs/facebook.svg";
+import twitter from "../images/svgs/twitter.svg";
+
+export const footerLinksData = [
+  {
+    title: "Contact Us",
+    url: "#",
+  },
+  {
+    title: "Privacy Policy",
+    url: "#",
+  },
+  {
+    title: "Terms of Service",
+    url: "#",
+  },
+];
+
+export const socialMediaLinks = [
+  {
+    name: "Twitter",
+    url: "https://twitter.com/",
+    icon: twitter,
+  },
+  {
+    name: "Facebook",
+    url: "https://facebook.com/",
+    icon: facebook,
+  },
+  {
+    name: "Instagram",
+    url: "https://instagram.com/",
+    icon: instagram,
+  },
+];
