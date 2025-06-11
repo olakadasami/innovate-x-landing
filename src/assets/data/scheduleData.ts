4334da9a7379a5729da31bc37774f0508bddcf91
Samuel Olakada <olakadasamuel@gmail.com>
2025-06-11 14:19:15 +0100

    chore: add schedule data

---
+++ src/assets/data/scheduleData.ts
@@ -0,0 +1,6 @@
+export const scheduleData = [
+  { day: 1, activity: "Main Event" },
+  { day: 2, activity: "Beginner Lesson 1" },
+  { day: 3, activity: "Beginner Lesson 2" },
+  { day: 4, activity: "Beginner Lesson 3" },
+];
