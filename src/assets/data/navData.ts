cd9ca8233af4a9a11f22b835272676c0357a4054
Samuel Olakada <olakadasamuel@gmail.com>
2025-06-13 12:55:44 +0100

    feat: add register link to google forms & #links for single page

--- src/assets/data/navData.ts
+++ src/assets/data/navData.ts
@@ -1,9 +1,12 @@
 export const navData = [
   { href: "/", title: "Logo" },
-  { href: "/schedule", title: "Schedule" },
-  { href: "/speakers", title: "Speakers" },
-  { href: "/sponsors", title: "Sponsors" },
-  { href: "/faq", title: "FAQ" },
-  { href: "/venue", title: "Venue" },
-  { href: "/register", title: "Register" },
+  { href: "/#schedule", title: "Schedule" },
+  { href: "/#speakers", title: "Speakers" },
+  { href: "/#sponsors", title: "Sponsors" },
+  { href: "/#faq", title: "FAQ" },
+  { href: "/#venue", title: "Venue" },
+  {
+    href: "https://docs.google.com/forms/d/e/1FAIpQLScuyeHDfdZMtVWemj1b5SljjhxB1dPHjIIJecJ3Mw3U0MVZNg/viewform?usp=sharing&ouid=101309478087224577359",
+    title: "Register",
+  },
 ];
